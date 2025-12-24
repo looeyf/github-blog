b0c5f610b20d99dd6d862edc488dcb9abe3db0f1
Luis Oliveira <luis10.pf@gmail.com>
2025-12-24 00:25:14 -0300

    chore: set api base url env variable

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -3,6 +3,7 @@ import axios from 'axios';
 const FIVE_SECONDS = 5000;
 
 const api = axios.create({
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   timeout: FIVE_SECONDS,
   headers: {
     'Content-Type': 'application/json',
