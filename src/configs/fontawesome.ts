78d3911bca6a1aa8be5e4e01345efeb48eb23a73
Luis Oliveira <luis10.pf@gmail.com>
2025-12-24 00:26:18 -0300

    feat: setup font awesome icons

---
+++ src/configs/fontawesome.ts
@@ -0,0 +1,18 @@
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import {
+  faBuilding,
+  faUserGroup,
+  faCalendarDay,
+  faComment,
+  faArrowUpRightFromSquare,
+} from '@fortawesome/free-solid-svg-icons';
+
+library.add(
+  faGithub,
+  faBuilding,
+  faUserGroup,
+  faCalendarDay,
+  faComment,
+  faArrowUpRightFromSquare,
+);
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './App.tsx';
+import './configs/fontawesome.ts';
 import './styles/reset.scss';
 import './styles/global.scss';
 
